3498b3f1d686a92e9bcfa536cddf9840d5217e69
lmraine <55120024+lmraine@users.noreply.github.com>
2025-01-16 12:12:36 -0600

    updated template

--- main.py
+++ main.py
@@ -2,10 +2,6 @@
 def build_dictionary(words):
     pass
 
-# REMOVE PASS AND FIX THE CODE TO:
-#1. asks for input, 
-#2. splits the input into a word list
-#3. calls build_dictionary() that will return a dictionary of each word in the input and it's frequency
-#4. output the contents of the dictionary sorted by key.
+# REMOVE PASS AND YOUR CODE GOES HERE
 if __name__ == '__main__':
     pass
